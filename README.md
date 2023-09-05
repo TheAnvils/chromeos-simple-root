c3f74764e89374acbddd80f8b2e2dd043a439309
TheAnvils <79940840+TheAnvils@users.noreply.github.com>
2023-09-05 19:24:07 +0000

    V1 finished

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # chromeos-simple-root
-Roots latest version of chromeos automatically. Regularly updated
+Roots a chromebook (or any other device) running chromeos. Check releases tab for more information about the current version. 
+# notes
+- This script assumes you are running it right after enabling developer mode so it may break if you have done the following: diable rootfs verification
+- Only works on latest versions of chromeos using android 10+
+# how and why to use
+First of all, other automated scripts didn't work for me so I made this. Now, you will need to enable developer mode (this will delete all your files!). After that install this apk: https://github.com/tiann/KernelSU/releases/download/v0.6.7/KernelSU_v0.6.7_11210-release.apk after installation hold ctrl+alt and press t to open the crosh shell. Type "shell"
+# credits
+Everything made by other people that was used here:
+- https://github.com/tiann/KernelSU
--- main.sh
+++ main.sh
@@ -1,13 +1,44 @@
+echo "Welcome to kernelSu rooting assistant 3000! Entering root shell"
+sudo bash
+sleep 5
+cd /home/chronos/user/Downloads
 if ! command -v wget &> /dev/null
 then
-    echo "wget is not installed."
+    echo "wget is not installed. Installing"
+    sleep 3
+    apt-get install wget
+    if ! command -v wget &> /dev/null
+    then
+    echo "critical error. Aborting"
     exit
+    fi
 fi
 if ! command -v unzip &> /dev/null
 then
-    echo "unzip is not installed."
+    echo "unzip is not installed. Installing"
+    sleep 3
+    apt-get install unzip
+    if ! command -v unzip &> /dev/null
+    then
+    echo "critical error. Aborting"
     exit
+    fi
 fi
+echo "Making the root filesystem writable..."
+sleep 3
+/usr/share/vboot/bin/make_dev_ssd.sh --remove_rootfs_verification
 
+echo "Downloading kernel..."
+sleep 1
 wget https://github.com/tiann/KernelSU/suites/15535949965/artifacts/886691235
 unzip kernel-ARCVM-x86_64-5.10.178.zip
+echo "Downloaded."
+sleep 3
+cd /opt/google/vms/android
+echo "Making kernel backup"
+mv vmlinux vmlinux.orig
+echo "IMPORTANT. Press ctrl+c to stop this script, otherwise it will continue in 5 seconds"
+sleep 5
+echo "Patching kernel"
+cp /home/chronos/user/Downloads/kernel-ARCVM-x86_64-5.10.178/bzImage ./vmlinux
+sudo reboot
